dffa3dd1242f31f74d4a528672ece03e66b3c600
Jason Dixon <jason@dixongroup.net>
2010-12-16 20:51:25 -0500

    sqlite schema, requires sqlite >= 3.6.19 for foreign key constraints

---
+++ db/site.sql
@@ -0,0 +1,59 @@
+PRAGMA foreign_keys = ON;
+CREATE TABLE users(
+   id INTEGER PRIMARY KEY,
+   role_id INTEGER,
+   username TEXT,
+   firstname TEXT,
+   lastname TEXT,
+   email TEXT,
+   url TEXT,
+   tz TEXT,
+   FOREIGN KEY(role_id) REFERENCES roles(id)
+);
+CREATE TABLE roles(
+   id INTEGER PRIMARY KEY,
+   name TEXT,
+   manage_admins BOOLEAN,
+   manage_editors BOOLEAN,
+   manage_users BOOLEAN,
+   edit_articles BOOLEAN,
+   delete_articles BOOLEAN,
+   create_articles BOOLEAN,
+   edit_comments BOOLEAN,
+   delete_comments BOOLEAN,
+   create_comments BOOLEAN,
+   read_articles BOOLEAN
+);
+CREATE TABLE articles(
+   id INTEGER PRIMARY KEY,
+   revision_id INTEGER,
+   status TEXT,
+   FOREIGN KEY(revision_id) REFERENCES revisions(id)
+);
+CREATE TABLE revisions(
+   article_id INTEGER,
+   user_id INTEGER,
+   date INTEGER, -- epoch
+   title TEXT,
+   dept TEXT,
+   format TEXT,
+   content TEXT,
+   FOREIGN KEY(article_id) REFERENCES articles(id),
+   FOREIGN KEY(user_id) REFERENCES users(id)
+);
+CREATE TABLE comments(
+   id INTEGER PRIMARY KEY,
+   article_id INTEGER,
+   user_id INTEGER,
+   date INTEGER, -- epoch
+   title TEXT,
+   content TEXT,
+   score INTEGER,
+   FOREIGN KEY(article_id) REFERENCES articles(id),
+   FOREIGN KEY(user_id) REFERENCES users(id)
+);
+CREATE TABLE activity(
+   id INTEGER PRIMARY KEY,
+   type TEXT,
+   message TEXT
+);
