0bcc1c7b1597a636bd2a16185afcb1be364980d9
obfuscurity <jason@dixongroup.net>
2011-01-03 05:53:52 +0000

    forgive me father for I have sinned, too long since my last commit

--- service.rb
+++ service.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'active_record'
 require 'sinatra'
-require 'haml'
+require 'erubis'
 
 # models
-require './models/user.rb'
+require "#{File.dirname(__FILE__)}/models/user.rb"
+require "#{File.dirname(__FILE__)}/models/role.rb"
 
 # setting up the environment
 env_index = ARGV.index('-e')
@@ -13,20 +14,53 @@ env = env_arg || ENV['SINATRA_ENV'] || 'development'
 databases = YAML.load_file("config/database.yml")
 ActiveRecord::Base.establish_connection(databases[env])
 
-set :haml, :escape_html => true
-set :show_exceptions, false
+set :erubis, :escape_html => true
+#set :show_exceptions, false
 
 get '/users' do
-  @users = User.find
-  haml 'users/list'.to_sym
+  @users = User.find(:all)
+  erubis 'users/list'.to_sym
 end
 
 get '/users/add' do
-  haml 'users/add'.to_sym
+  erubis 'users/add'.to_sym
 end
 
-get '/users/:name' do
-  user = User.find_by_name(params[:name])
-  haml 'users/profile'.to_sym
+get '/users/:username' do
+  @user = User.find_by_username(params[:username])
+  if @user
+    erubis 'users/profile'.to_sym
+  else
+    erubis 'errors/404'.to_sym
+  end
+end
+
+post '/users/add' do
+  role = Role.find_by_name('normal')
+  user = User.create_unless_user_exists({
+    :role_id => role.id,
+    :username => params[:username],
+    :password => params[:password1],
+    :firstname => params[:firstname],
+    :lastname => params[:lastname],
+    :email => params[:email],
+    :url => params[:url],
+    :tz => params[:tz],
+  })
+  if user.nil?
+    @error = 'Username or email already exists, please try again'
+    erubis 'users/add'.to_sym
+  else
+    redirect '/users'
+  end
+end
+
+get %r{^/$} do
+  @articles = Article.find_by_status('active')
+  if @articles
+    erubis 'articles/detail'.to_sym
+  else
+    puts 'no articles yet!'
+  end
 end
 
